cda4c9eb882838de626111f9c67416bd139d2705
Kaan Koç <kock@mef.edu.tr>
2023-12-16 12:08:28 +0300

    fix: :) rby

--- gitflow-helper.rb
+++ gitflow-helper.rb
@@ -1,15 +1,16 @@
 class GitFlowHelper < Formula
-    desc: "Creating a hotfix using gitflow. Also it can create pr or output with lolcat if client has that packages."
-    homepage: "https://github.com/kaanmertkoc/gitflow-helper-shell"
-    url: "https://github.com/kaanmertkoc/gitflow-helper-shell/archive/refs/heads/master.zip"
-    sha256: "SHA256-OF-TARBALL"
-    license: "MIT"
-
+    desc "Creating a hotfix using gitflow. Also it can create pr or output with lolcat if client has that packages."
+    homepage "https://github.com/kaanmertkoc/gitflow-helper-shell"
+    url "https://github.com/kaanmertkoc/gitflow-helper-shell/archive/refs/heads/master.zip"
+    sha256 "SHA256-OF-TARBALL" # Replace with actual SHA256 checksum of your tarball
+    license "MIT"
+  
     def install
-        bin.install "hotfix.sh"
+      bin.install "hotfix.sh"
     end
-
+  
     test do
-        system "#{bin}/hotfix.sh", "--version"
+      system "#{bin}/hotfix.sh", "--version"
     end
-end
+  end
+  

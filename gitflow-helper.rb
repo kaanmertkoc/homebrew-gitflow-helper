b31a5a04011cb1f77e1a76291dc13a7fe7728550
Kaan Koç <kock@mef.edu.tr>
2023-12-16 12:41:07 +0300

    fix: checksum

--- gitflow-helper.rb
+++ gitflow-helper.rb
@@ -2,7 +2,7 @@ class GitflowHelper < Formula
     desc "Creating a hotfix using gitflow. Also it can create pr or output with lolcat if client has that packages."
     homepage "https://github.com/kaanmertkoc/gitflow-helper-shell"
     url "https://github.com/kaanmertkoc/gitflow-helper-shell/archive/refs/heads/master.zip"
-    sha256 "e28f8c8efdcd0bdc4ba211869fe8de0695de347d2813cd5243d09506f94c9bba" # Replace with actual SHA256 checksum of your tarball
+    sha256 "d048bee190cfa92b44b9c152ca401aca796b7b4720fd4b246a25f9dcd0400210" # Replace with actual SHA256 checksum of your tarball
     license "MIT"
     version "1.0.1" # Replace with your desired version number
   

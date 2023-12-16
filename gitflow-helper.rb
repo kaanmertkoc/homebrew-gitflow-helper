53b36f925824c753b2e705d85d0e5ef21a98cbde
Kaan Koç <kock@mef.edu.tr>
2023-12-16 11:58:45 +0300

    git flow helper made

---
+++ gitflow-helper.rb
@@ -0,0 +1,15 @@
+class GitFlowHelper < Formula
+    desc: "Creating a hotfix using gitflow. Also it can create pr or output with lolcat if client has that packages.",
+    homepage: "https://github.com/kaanmertkoc/gitflow-helper-shell"
+    url: "https://github.com/kaanmertkoc/gitflow-helper-shell/archive/refs/heads/master.zip"
+    sha256: "SHA256-OF-TARBALL"
+    license "MIT"
+
+    def install
+        bin.install "hotfix.sh"
+    end
+
+    test do
+        system "#{bin}/hotfix.sh", "--version"
+    end
+end

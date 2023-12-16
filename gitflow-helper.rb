b15febd166f9791e8023c462c7195144bbd1963b
Kaan Koç <kock@mef.edu.tr>
2023-12-16 21:21:38 +0300

    feat: add hotfix finish and release finish

--- gitflow-helper.rb
+++ gitflow-helper.rb
@@ -4,16 +4,20 @@ class GitflowHelper < Formula
     url "https://github.com/kaanmertkoc/gitflow-helper-shell/archive/refs/heads/master.zip"
     sha256 "d048bee190cfa92b44b9c152ca401aca796b7b4720fd4b246a25f9dcd0400210" # Replace with actual SHA256 checksum of your tarball
     license "MIT"
-    version "1.0.1" # Replace with your desired version number
+    version "1.0.2" # Replace with your desired version number
   
     def install
       bin.install "hotfix.sh" => "hotfix"
+      bin.install "hotfix_finish.sh" => "hotfix finish"
       bin.install "release.sh" => "release"
+      bin.install "release_finish.sh" => "release finish"
     end
   
     test do
       system "#{bin}/hotfix", "--version"
       system "#{bin}/release", "--version"
+      system "#{bin}/hotfix finish", "--version"
+      system "#{bin}/release finish", "--version"
     end
   end
   
